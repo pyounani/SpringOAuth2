b985c68e6d99885c2ce3bf6eec3bb74baccf0204
pyounani <yndbsk9372@gmail.com>
2024-04-13 22:04:52 +0900

    [feat] 유저 정보 DB 저장 로지 추가

--- src/main/java/com/example/springOAuth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/springOAuth2/service/CustomOAuth2UserService.java
@@ -4,6 +4,9 @@ import com.example.springOAuth2.dto.CustomOAuth2User;
 import com.example.springOAuth2.dto.GoogleResponse;
 import com.example.springOAuth2.dto.NaverResponse;
 import com.example.springOAuth2.dto.OAuth2Response;
+import com.example.springOAuth2.entity.UserEntity;
+import com.example.springOAuth2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -13,7 +16,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -33,7 +39,25 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             return null;
         }
 
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
+
         String role = "ROLE_USER";
+        if (existData == null) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole(role);
+
+            userRepository.save(userEntity);
+        } else {
+            existData.setUsername(username);
+            existData.setEmail(oAuth2Response.getEmail());
+
+            role = existData.getRole();
+
+            userRepository.save(existData);
+        }
 
         return new CustomOAuth2User(oAuth2Response, role);
     }
